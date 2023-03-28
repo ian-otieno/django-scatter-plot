3665f48532667f75326e9714724a97bc770b79b7
iotieno <iotieno@fintech-group.com>
2023-03-28 15:58:57 +0300

    edited views

--- Plot/views.py
+++ Plot/views.py
@@ -10,6 +10,17 @@ def home(request):
 def scatter_view(request):
     x = [1, 2, 3]
     y = [4, 2, 5]
+    
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    mode_x = statistics.mode(x)
+    mode_y = statistics.mode(y)
+    median_x = np.median(x)
+    median_y = np.median(y)
+    std_x = np.std(x)
+    std_y = np.std(y)
+    corr_coef = np.corrcoef(x, y)[0][1]
+    
     data = [
         go.Scatter(
             x=x,
@@ -17,10 +28,24 @@ def scatter_view(request):
             mode='markers'
         )
     ]
+    
     layout = go.Layout(
-        title=f'Scatter Plot - Mean: {np.mean(x):.2f},{np.mean(y):.2f}; Mode: {statistics.mode(x)},{statistics.mode(y)}; Median: {np.median(x)},{np.median(y)}; Standard Deviation: {np.std(x):.2f},{np.std(y):.2f}; Coefficient of Correlation: {np.corrcoef(x, y)[0][1]:.2f}'
+        title=f'Scatter Plot'
     )
+    
     fig = go.Figure(data=data, layout=layout)
     plot_div = plot(fig, output_type='div')
-    return render(request, 'scatter.html', {'plot_div': plot_div})
-
+    
+    stats = {
+        'mean_x': mean_x,
+        'mean_y': mean_y,
+        'mode_x': mode_x,
+        'mode_y': mode_y,
+        'median_x': median_x,
+        'median_y': median_y,
+        'std_x': std_x,
+        'std_y': std_y,
+        'corr_coef': corr_coef
+    }
+    
+    return render(request, 'scatter.html', {'plot_div': plot_div, 'stats': stats})
