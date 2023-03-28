ab6a1002350759fd25386808f270cdee9f248501
iotieno <iotieno@fintech-group.com>
2023-03-28 14:08:24 +0300

    edited views

--- Plot/views.py
+++ Plot/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 import plotly.graph_objs as go
 from plotly.offline import plot
-
-
+import numpy as np
+import statistics
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-
 def scatter_view(request):
+    x = [1, 2, 3]
+    y = [4, 2, 5]
     data = [
         go.Scatter(
-            x=[1, 2, 3],
-            y=[4, 2, 5],
+            x=x,
+            y=y,
             mode='markers'
         )
     ]
     layout = go.Layout(
-        title='Scatter Plot'
+        title=f'Scatter Plot - Mean: {np.mean(x):.2f},{np.mean(y):.2f}; Mode: {statistics.mode(x)},{statistics.mode(y)}; Median: {np.median(x)},{np.median(y)}; Standard Deviation: {np.std(x):.2f},{np.std(y):.2f}; Coefficient of Correlation: {np.corrcoef(x, y)[0][1]:.2f}'
     )
     fig = go.Figure(data=data, layout=layout)
     plot_div = plot(fig, output_type='div')
-    return render(request, 'scatter.html', {'plot_div': plot_div})    
+    return render(request, 'scatter.html', {'plot_div': plot_div})
 
